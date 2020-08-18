5d7afe83fd99b789335f8e66675d743a26befa0c
unknown <michal.sopel1@gmail.com>
2020-08-18 20:34:16 +0200

    refactoring and new func

--- develop/js/Table.js
+++ develop/js/Table.js
@@ -3,35 +3,72 @@ import React, { useEffect } from "react";
 const Table = () => {
   useEffect(() => {
     const memoryCard = document.querySelectorAll(".memory-card");
+
     let fCard,
       sCard,
-      isClicked = false;
+      isClicked = false,
+      cardLocker = false;
 
     function flipAnimation() {
-      this.classList.add("flipingCard");
+      if (cardLocker) return;
+      //Locking double click
+      if (this === fCard) return;
+      this.classList.add("flippingCard");
       //Sprawdzenie czy pierwsza karta została wybrana
       //Check the first card was selected
       if (!isClicked) {
         isClicked = true;
         fCard = this;
       } else {
-        isClicked = false;
         sCard = this;
-        //Łączenie kart
-        //Matching cards
-        if (fCard.dataset.fruitname === sCard.dataset.fruitname) {
-          fCard.removeEventListener("click", flipAnimation);
-          sCard.removeEventListener("click", flipAnimation);
-        } else {
-          //Usunięcie klasy jeśli nie pasują
-          //If they don't match, removing flip class
-          setTimeout(() => {
-            fCard.classList.remove("flipingCard");
-            sCard.classList.remove("flipingCard");
-          }, 800);
-        }
+        cardMatching();
       }
     }
+
+    function cardMatching() {
+      //Łączenie kart
+      //Matching cards
+      if (fCard.dataset.fruitname === sCard.dataset.fruitname) {
+        listenerRemover();
+      } else {
+        backRotating();
+      }
+    }
+
+    function listenerRemover() {
+      fCard.removeEventListener("click", flipAnimation);
+      sCard.removeEventListener("click", flipAnimation);
+      clearMemory();
+    }
+
+    function backRotating() {
+      //Blokada kart
+      //Cards blocking
+      cardLocker = true;
+      //Usunięcie klasy jeśli nie pasują
+      //If they don't match, removing flip class
+      //delay 800ms
+      setTimeout(() => {
+        fCard.classList.remove("flippingCard");
+        sCard.classList.remove("flippingCard");
+        //unlocking cards
+        clearMemory();
+      }, 800);
+    }
+
+    function clearMemory() {
+      fCard = null;
+      sCard = null;
+      cardLocker = false;
+      isClicked = false;
+    }
+
+    (function shuffleCards() {
+      memoryCard.forEach((card) => {
+        let randomCardsPos = Math.floor(Math.random() * 19);
+        card.style.order = randomCardsPos;
+      });
+    })();
     memoryCard.forEach((card) => card.addEventListener("click", flipAnimation));
   }, []);
   return (
